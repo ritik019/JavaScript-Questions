341105f5984793f52e1c607ac35ae23e50a3cab6
Sakshyat Pradhan <111267468+ritik019@users.noreply.github.com>
2025-02-18 00:46:26 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,16 @@
+// for online compiler
+
+let inputStr = prompt("Enter the input: ");
+let reversedStr = reverseStr(inputStr);
+console.log(reversedStr);
+
+function reverseStr(str) {
+    return str.split("").reverse().join("");
+}
+
+
+// if we give our input
+console.log(reverseStr("Sakshyat"));
+function reverseStr(str) {
+    return str.split("").reverse().join("");
+}
